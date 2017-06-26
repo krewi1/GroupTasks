744a7181f634db8c76618647ffb67f1f4389b2ce
p.kratochvil@post.cz <krewi1>
2017-06-26 11:20:20 +0200

    user demo

--- client/components/general/app.js
+++ client/components/general/app.js
@@ -1,25 +1,45 @@
 import React, {Component} from 'react';
 import {graphql, withApollo} from 'react-apollo';
+import {hashHistory} from 'react-router';
 
 import SideBar from './side_bar.js';
 import Loader from './loader.js';
 import {query} from '../../queries/queries.js'
 
 class App extends Component {
-    componentWillReceiveProps(){
-        debugger;
-        const data = this.props.data;
-        if(!data.profile){
-            data.refetch();
+    constructor(props) {
+        super();
+        this.state = {
+            selected: props.params.id
         }
     }
+
+    componentWillReceiveProps() {
+        debugger;
+        const data = this.props.data;
+        const selected = this.state.selected;
+
+        data.refetch({
+            variables: {
+                userId: selected
+            }
+        });
+    }
+
+    changeUser = (event) => {
+        this.setState({selected: event.target.value});
+        hashHistory.push(`/${event.target.value}`);
+    };
+
     render() {
         const data = this.props.data;
-        if(data.loading){return <Loader />;}
+        if (data.loading) {
+            return <Loader />;
+        }
 
         return (
             <div className="container">
-                <SideBar />
+                <SideBar changeUser={this.changeUser} selected={this.state.selected}/>
                 <div>{React.cloneElement(this.props.children, {user: data.profile})}</div>
             </div>
         );
@@ -28,5 +48,5 @@ class App extends Component {
 
 
 export default graphql(query, {
-    options: ({ params }) => ({ variables: { userId: params.id || 1 } })
+    options: ({params}) => ({variables: {userId: params.id || 1}})
 })(App)
--- client/components/general/side_bar.js
+++ client/components/general/side_bar.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import {Link} from 'react-router';
 
-const SideBar = () => {
+const SideBar = (props) => {
+    debugger;
     return (
-        <ul className="side-nav fixed">
-            <li><div className="userView">
-                <div className="background">
-                </div>
-                <a href="#!name"><span className="white-text name">John Doe</span></a>
-                <a href="#!email"><span className="white-text email">jdandturk@gmail.com</span></a>
-            </div></li>
-            <li><Link to="/profile"><i className="material-icons">perm_identity</i>Profile</Link></li>
-            <li><Link to="/opengroups"><i className="material-icons">lock_open</i>OpenGroups</Link></li>
-            <li><Link to="/my-events"><i className="material-icons">group_work</i>MyGroups</Link></li>
-        </ul>
+        <div className="side-nav fixed">
+            <label>Browser Select</label>
+            <select className="browser-default"
+                    onChange={props.changeUser}
+                    value={props.selected}>
+                <option value="1">Admin user</option>
+                <option value="2">Normal user</option>
+            </select>
+            <ul >
+                <li>
+                    <div className="userView">
+                        <div className="background">
+                        </div>
+                        <a href="#!name"><span className="white-text name">John Doe</span></a>
+                        <a href="#!email"><span className="white-text email">jdandturk@gmail.com</span></a>
+                    </div>
+                </li>
+                <li><Link to="/profile"><i className="material-icons">perm_identity</i>Profile</Link></li>
+                <li><Link to="/opengroups"><i className="material-icons">lock_open</i>OpenGroups</Link></li>
+                <li><Link to="/my-events"><i className="material-icons">group_work</i>MyGroups</Link></li>
+            </ul>
+        </div>
     );
 };
 
